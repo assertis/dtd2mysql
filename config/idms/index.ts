5764b34373e51ba2d94a087f7c2fe62fa7adc2bf
Michał Tatarynowicz <tatarynowicz@gmail.com>
2019-03-12 12:10:32 +0100

    JP-464 Switch IDMS fixed link source from IDMS' bucket to Ride's (#6)

--- config/idms/index.ts
+++ config/idms/index.ts
@@ -2,7 +2,9 @@
 import FixedLinks_v10 from "./file/FixedLinks_v1.0";
 import {FeedConfig} from "../index";
 
-export const idmsFixedLinksDownloadUrl = "https://s3-eu-west-1.amazonaws.com/idms/FixedLinks_v1.0.xml";
+export const idmsFixedLinksBucket = "ride-data";
+export const idmsFixedLinksPath = "idms/FixedLinks_v1.0.xml";
+export const idmsFixedLinkUrl = "https://s3-eu-west-1.amazonaws.com/" + idmsFixedLinksBucket + "/" + idmsFixedLinksPath;
 
 const specification: FeedConfig = {
   "": FixedLinks_v10
