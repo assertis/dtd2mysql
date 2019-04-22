6bea722e0e1305eb1d4b224fc2874068863c7dc2
Dyktus <nowak.lukasz93@gmail.com>
2019-04-22 21:12:44 +0200

    Process data old way

--- src/dev.ts
+++ src/dev.ts
@@ -1,7 +1,22 @@
 
 
-const r  = new RegExp(/^fares_/);
+const TABLES = [
+  'additional_fixed_link',
+  'alias',
+  'association',
+  'fixed_link',
+  'idms_fixed_link',
+  'log',
+  'nfm64',
+  'physical_station',
+  'schedule',
+  'schedule_extra',
+  'stop_time',
+  'tiploc',
+  'z_schedule',
+  'z_stop_time',
+];
 
-console.log(
-  'fares_01_01'.match(r)
-)
+TABLES.map(t => {
+  console.log(`RENAME TABLE timetable_15_04.${t} TO timetable.${t};`);
+});
