d394eb19d4cb056fb8639e7cba62f3a749b57b31
Luke <nowak.lukasz93@gmail.com>
2019-11-07 07:36:55 +0100

    Remove unused method

--- src/tp/TransferPatternCommand.ts
+++ src/tp/TransferPatternCommand.ts
@@ -24,19 +24,4 @@ export class TransferPatternCommand implements CLICommand {
             Payload: Buffer.from(""),
         }
     }
-
-    private get testMail() {
-        return {
-            FunctionName: "mailingService-send-dev",
-            InvocationType: "Event",
-            LogType: "Tail",
-            Payload: Buffer.from(JSON.stringify({
-                from: "Mailgun Sandbox <postmaster@assertis.co.uk>",
-                to: "Bartlomiej Olewinski <lukasz.nowak@assertis.co.uk>",
-                subject: "Test skryptu",
-                text: "Congratulations Bartlomiej Olewinski, you just sent an email with Mailgun!  You are truly awesome!",
-                html: "<strong>This is an example of message send via API Gateway</strong> Let`s see how <i>it will look like</i><p>Next paragraph</p>"
-            })),
-        }
-    }
 }
