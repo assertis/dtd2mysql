4b26db2971ce823174833bd47e7efcedb8d5716b
Luke <nowak.lukasz93@gmail.com>
2019-11-15 11:01:51 +0100

    TGA-4 Check fares and timetable files availability command

--- src/cli/CheckAvailableFilesCommand.ts
+++ src/cli/CheckAvailableFilesCommand.ts
@@ -7,33 +7,36 @@ export class CheckAvailableFilesCommand implements CLICommand  {
 
     public constructor(
         private readonly ftpSource: PromiseSFTP,
-        private readonly faresFileManager: SourceManager,
-        private readonly timetableFileManager: SourceManager
+        private readonly faresSource: SourceManager,
+        private readonly timetableSource: SourceManager
     ) {}
 
     public async run(argv: string[]): Promise<any> {
         let lastProcessedFile, allFiles;
         // Fares
-        lastProcessedFile = await this.faresFileManager.getLastProcessedFile();
-        allFiles = await this.faresFileManager.getRemoteFiles(faresPath);
-        const fares = await this.faresFileManager.getFilesToProcess(allFiles, lastProcessedFile);
+        lastProcessedFile = await this.faresSource.getLastProcessedFile();
+        allFiles = await this.faresSource.getRemoteFiles(faresPath);
+        const fares = await this.faresSource.getFilesToProcess(allFiles, lastProcessedFile);
         // Timetables
-        lastProcessedFile = await this.timetableFileManager.getLastProcessedFile();
-        allFiles = await this.timetableFileManager.getRemoteFiles(timetablePath);
-        const timetables = await this.timetableFileManager.getFilesToProcess(allFiles, lastProcessedFile);
+        lastProcessedFile = await this.timetableSource.getLastProcessedFile();
+        allFiles = await this.timetableSource.getRemoteFiles(timetablePath);
+        const timetables = await this.timetableSource.getFilesToProcess(allFiles, lastProcessedFile);
 
         if(fares.length > 0 && timetables.length > 0) {
             await this.end();
             console.log("Process the data");
         } else {
             await this.end();
+            /**
+             * We throw to stop executing any other command after check availability.
+             */
             throw new Error("No data to update");
         }
     }
 
 
     private async end() {
-        await this.faresFileManager.end();
-        await this.timetableFileManager.end();
+        await this.faresSource.end();
+        await this.timetableSource.end();
     }
 }
--- src/cli/DownloadCommand.ts
+++ src/cli/DownloadCommand.ts
@@ -4,7 +4,7 @@ import {SourceManager} from "../sftp/SourceManager";
 export class DownloadCommand implements CLICommand {
 
   constructor(
-    private readonly fileManager: SourceManager,
+    private readonly sourceManager: SourceManager,
     private readonly directory: string
   ) {}
 
@@ -14,11 +14,11 @@ export class DownloadCommand implements CLICommand {
   public async run(argv: string[]): Promise<string[]> {
     const outputDirectory = argv[3] || "/tmp/";
     const [remoteFiles, lastProcessedFile] = await Promise.all([
-      this.fileManager.getRemoteFiles(this.directory),
-      this.fileManager.getLastProcessedFile()
+      this.sourceManager.getRemoteFiles(this.directory),
+      this.sourceManager.getLastProcessedFile()
     ]);
 
-    const files = this.fileManager.getFilesToProcess(remoteFiles, lastProcessedFile);
+    const files = this.sourceManager.getFilesToProcess(remoteFiles, lastProcessedFile);
 
     if (files.length > 0) {
       console.log(`Downloading ${files.length} feed file(s)`);
@@ -29,14 +29,14 @@ export class DownloadCommand implements CLICommand {
 
     try {
       await Promise.all(
-        files.map(f => this.fileManager.sftp.fastGet(this.directory + f, outputDirectory + f))
+        files.map(f => this.sourceManager.sftp.fastGet(this.directory + f, outputDirectory + f))
       );
     }
     catch (err) {
       console.error(err);
     }
 
-    this.fileManager.sftp.end();
+    this.sourceManager.sftp.end();
 
     return files.map(filename => outputDirectory + filename);
   }
