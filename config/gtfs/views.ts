d1a7acbdeb382b4f7e3b3b10c0c148a657ce9bbc
Luke <nowak.lukasz93@gmail.com>
2020-07-13 10:52:51 +0200

    Revert

---
+++ config/gtfs/views.ts
@@ -0,0 +1,20 @@
+export const GTFS_TABLES = [
+    'agency',
+    'calendar',
+    'calendar_dates',
+    'links',
+    'routes',
+    'shapes',
+    'stop_times',
+    'stops',
+    'transfers',
+    'trips'
+];
+
+export const ojpViews = 'START TRANSACTION;' +
+    GTFS_TABLES.map(j => `
+  DROP TABLE IF EXISTS {orgdb}.${j};
+  CREATE OR REPLACE VIEW {orgdb}.${j} AS SELECT * FROM {dbname}.${j};
+  `).join('') +
+    'CREATE OR REPLACE VIEW {orgdb}.transfer_patterns AS SELECT * FROM transfer_patterns.transfer_patterns;'
+' COMMIT;';
