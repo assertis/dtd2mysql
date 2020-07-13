5c3187afa63251b67692f40c959ee47595626971
Michał Tatarynowicz <tatarynowicz@gmail.com>
2020-07-13 09:55:16 +0200

    Fix to AddLateNightServices breaking if first calling point has no departure time.

--- src/gtfs/command/AddLateNightServices.ts
+++ src/gtfs/command/AddLateNightServices.ts
@@ -9,18 +9,27 @@ export function addLateNightServices(schedules: Schedule[], idGenerator: IdGener
 
   for (const schedule of schedules) {
     result.push(schedule);
-    const departureHour = parseInt(schedule.stopTimes[0].departure_time.substr(0, 2), 10);
 
-    if (departureHour <= 1) {
-      const newSchedule = schedule.clone(schedule.calendar.shiftBackward(), idGenerator.next().value);
+    const departure = schedule.stopTimes[0].departure_time;
 
-      for (const stop of newSchedule.stopTimes) {
-        stop.departure_time = (parseInt(stop.departure_time.substr(0, 2), 10) + 24) + stop.departure_time.substr(2);
-        stop.arrival_time = (parseInt(stop.arrival_time.substr(0, 2), 10) + 24) + stop.arrival_time.substr(2);
-      }
-
-      result.push(newSchedule);
+    if (!departure) {
+      continue;
     }
+
+    const departureHour = parseInt(departure.substr(0, 2), 10);
+
+    if (departureHour > 1) {
+      continue;
+    }
+
+    const newSchedule = schedule.clone(schedule.calendar.shiftBackward(), idGenerator.next().value);
+
+    for (const stop of newSchedule.stopTimes) {
+      stop.departure_time = (parseInt(stop.departure_time.substr(0, 2), 10) + 24) + stop.departure_time.substr(2);
+      stop.arrival_time = (parseInt(stop.arrival_time.substr(0, 2), 10) + 24) + stop.arrival_time.substr(2);
+    }
+
+    result.push(newSchedule);
   }
 
   return result;
--- test/gtfs/command/AddLateNightServices.spec.ts
+++ test/gtfs/command/AddLateNightServices.spec.ts
@@ -39,6 +39,37 @@ describe("AddLateNightServices", () => {
     chai.expect(schedules[2].calendar.runsTo.isSame("20181031")).to.be.true;
   });
 
+  it("merges schedules where they are the same", () => {
+    const baseSchedules = [
+      schedule(1, "A", "2018-10-01", "2018-10-31", STP.Permanent, WEEK_DAYS, [
+        stop(1, "TON", "01:30"),
+        stop(2, "PDW", "01:40"),
+        stop(3, "ASH", "01:50")
+      ]),
+      schedule(2, "B", "2018-10-01", "2018-10-31", STP.Permanent, WEEK_DAYS, [
+        stop(1, "TON", "02:30"),
+        stop(2, "PDW", "02:40"),
+        stop(3, "ASH", "02:50")
+      ]),
+    ];
+
+    const schedules = addLateNightServices(baseSchedules, idGenerator());
+
+    chai.expect(schedules[0].calendar.runsFrom.isSame("20181001")).to.be.true;
+    chai.expect(schedules[0].calendar.runsTo.isSame("20181031")).to.be.true;
+    chai.expect(schedules[1].calendar.runsFrom.isSame("20180930")).to.be.true;
+    chai.expect(schedules[1].calendar.runsTo.isSame("20181030")).to.be.true;
+    chai.expect(schedules[1].calendar.days[0]).to.equal(1);
+    chai.expect(schedules[1].calendar.days[1]).to.equal(1);
+    chai.expect(schedules[1].calendar.days[2]).to.equal(1);
+    chai.expect(schedules[1].calendar.days[3]).to.equal(1);
+    chai.expect(schedules[1].calendar.days[4]).to.equal(0);
+    chai.expect(schedules[1].calendar.days[5]).to.equal(0);
+    chai.expect(schedules[1].calendar.days[6]).to.equal(1);
+    chai.expect(schedules[2].calendar.runsFrom.isSame("20181001")).to.be.true;
+    chai.expect(schedules[2].calendar.runsTo.isSame("20181031")).to.be.true;
+  });
+
 });
 
 function *idGenerator(): IterableIterator<number> {
